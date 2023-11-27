0205ca777c0be52025647b91d0f0430b1e05dc75
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-27 20:14:11 +0000

    New problem statement in comment

--- recursive_function_example.py
+++ recursive_function_example.py
@@ -11,7 +11,20 @@ def sum_up_to(upper_limit):
 # Write a function that multiplies all the numbers up to the one supplied
 
 
+def product_up_to(upper_limit):
+    if upper_limit == 1:
+        return 1
+    return upper_limit * product_up_to(upper_limit - 1)
+
+
+# Function that
+#  * take a string
+#  * returns True if it's a palindrome
+#  * returns False otherwise
+
+
 if __name__ == "__main__":
     result = sum_up_to(6) # 1 + 2 + 3 + 4 + 5 + 6
     print(result) # 21
-    print('HELLO')
+    result = product_up_to(6) # 1 * 2 * 3 * 4 * 5 * 6
+    print(result) # 720
