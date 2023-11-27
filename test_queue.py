62776f2e7e95b630e29e751062e6998ee9cc5d0f
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-27 19:00:41 +0000

    FIFO test

--- test_queue.py
+++ test_queue.py
@@ -5,6 +5,17 @@ from queue import Queue
 class TestQueue(unittest.TestCase):
     def test_can_initialize(self):
         queue = Queue()
+        self.assertIsNotNone(queue)
+
+    def test_popping_is_fifo(self):
+        queue = Queue()
+        queue.push(1)
+        queue.push(2)
+        queue.push(3)
+
+        self.assertEqual(queue.pop(), 1)
+        self.assertEqual(queue.pop(), 2)
+        self.assertEqual(queue.pop(), 3)
 
 
 if __name__ == '__main__':
