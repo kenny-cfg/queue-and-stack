7a1f5cb615cec857badac780efa8c6164890fd07
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-27 20:02:07 +0000

    Factorial problem in a comment

--- recursive_function_example.py
+++ recursive_function_example.py
@@ -1,10 +1,17 @@
+# Recursion
+# * Starting point (typically, i = 0, or empty string, or empty collection
+# * Code to reduce any other value to a smaller value (typically,
+#   reduce the argument, or a shorter string, or a smaller collection
+
 def sum_up_to(upper_limit):
-    sum = 0
-    for i in range(upper_limit + 1):
-        sum += i
-    return sum
+    if upper_limit == 0:
+        return 0
+    return upper_limit + sum_up_to(upper_limit - 1)
+
+# Write a function that multiplies all the numbers up to the one supplied
 
 
 if __name__ == "__main__":
     result = sum_up_to(6) # 1 + 2 + 3 + 4 + 5 + 6
     print(result) # 21
+    print('HELLO')
