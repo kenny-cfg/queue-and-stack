5c77f028970a04a25ad54cde87a3238eca1c8e48
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-27 19:05:07 +0000

    Failing instantiation test

--- queue.py
+++ queue.py
@@ -4,10 +4,10 @@ class Queue:
     def __init__(self):
         self.storage = []
 
-    def push(self, element):
+    def enqueue(self, element):
         self.storage.append(element)
 
-    def pop(self):
+    def dequeue(self):
         value = self.storage[0]
         self.storage = self.storage[1::]
         return value
--- test_queue.py
+++ test_queue.py
@@ -9,13 +9,20 @@ class TestQueue(unittest.TestCase):
 
     def test_popping_is_fifo(self):
         queue = Queue()
-        queue.push(1)
-        queue.push(2)
-        queue.push(3)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
 
-        self.assertEqual(queue.pop(), 1)
-        self.assertEqual(queue.pop(), 2)
-        self.assertEqual(queue.pop(), 3)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.dequeue(), 2)
+        self.assertEqual(queue.dequeue(), 3)
+
+    def test_can_instantiate_with_initial_values(self):
+        queue = Queue(1, 2, 3)
+
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.dequeue(), 2)
+        self.assertEqual(queue.dequeue(), 3)
 
 
 if __name__ == '__main__':
